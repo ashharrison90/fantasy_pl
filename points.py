a29b34ea6865a42d4b022228aad593eaac231396
ashharrison90 <ashharrison90@gmail.com>
2016-08-13 21:41:08 +0100

    fix fixture multiplier

--- points.py
+++ points.py
@@ -13,11 +13,11 @@ def predict_points(json_object, json_fixture_object):
         expected_points = float(form)
     else:
         expected_points = float(ppg)
-    injury_ratio = get_injury_multiplier(json_object)
-    fixture_ratio = get_fixture_multiplier(json_fixture_object)
+    injury_ratio = calculate_injury_multiplier(json_object)
+    fixture_ratio = calculate_fixture_multiplier(json_fixture_object)
     return expected_points * injury_ratio * fixture_ratio
 
-def get_injury_multiplier(json_object):
+def calculate_injury_multiplier(json_object):
     """Given a player's json object, extract an injury/suspension ratio.
     This will act as a multiplier for the expected points. For example, if a player
     is expected to score 10 points, but is only 50% likely to play, the expected points
@@ -35,7 +35,7 @@ def get_injury_multiplier(json_object):
             injury_ratio = int(search_matched.group(1)) / 100
     return injury_ratio
 
-def get_fixture_multiplier(json_fixture_object):
+def calculate_fixture_multiplier(json_fixture_object):
     """Given a player's json fixture object, calculate a fixture multiplier for
     the expected points. For example, if a player has an easier game (e.g. home
     to Mboro), 'difficulty' would be 1, which we convert to a multiplier which is
@@ -45,6 +45,6 @@ def get_fixture_multiplier(json_fixture_object):
     # difficulty is 1-5, 5 being the hardest
     # we convert this to a ratio adjustment as follows:
     #    1    2  3    4    5
-    # -0.2 -0.1 -0 +0.1 +0.2
-    normalised_adjustment = (difficulty - 3) / 10
+    # +0.2 +0.1 +0 -0.1 -0.2
+    normalised_adjustment = (3 - difficulty) / 10
     return 1 + normalised_adjustment
