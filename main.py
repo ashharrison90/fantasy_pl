59610360bc8216b87e94a987fb6d0a33b558032a
ashharrison90 <ashharrison90@gmail.com>
2016-10-16 19:06:02 +0100

    Added commented lines back in - whoops :|

--- main.py
+++ main.py
@@ -27,13 +27,13 @@ CURRENT_SQUAD = web_service.get_transfers_squad()
 # Calculate the new squad
 NEW_SQUAD = linear_solver.select_squad(CURRENT_SQUAD)
 
-# # make transfers to update the squad on fantasy.premierleague.com
-# TRANSFER_OBJECT = web_service.create_transfers_object(
-#     CURRENT_SQUAD['picks'], NEW_SQUAD)
-# web_service.make_transfers(TRANSFER_OBJECT)
-#
-# # Calculate the new starting lineup
-# NEW_STARTING = linear_solver.select_starting(NEW_SQUAD)
-#
-# # update the starting lineup on fantasy.premierleague.com
-# web_service.set_starting_lineup(NEW_STARTING)
+# make transfers to update the squad on fantasy.premierleague.com
+TRANSFER_OBJECT = web_service.create_transfers_object(
+    CURRENT_SQUAD['picks'], NEW_SQUAD)
+web_service.make_transfers(TRANSFER_OBJECT)
+
+# Calculate the new starting lineup
+NEW_STARTING = linear_solver.select_starting(NEW_SQUAD)
+
+# update the starting lineup on fantasy.premierleague.com
+web_service.set_starting_lineup(NEW_STARTING)
