bf0fe51f4269122529821d3114225b222d1d6e7e
Ashley Harrison <ashley.harrison@uk.ibm.com>
2018-08-01 17:12:14 +0100

    Add calculate_past_fixture_multiplier, to prevent players who never play being picked because of high ppg

--- points.py
+++ points.py
@@ -23,13 +23,14 @@ def predict_points(json_object, json_fixture_object, gameweek=0):
     how many points a given player will score in the next gameweek.
     We use the highest out of 'form' and 'points_per_game'
     """
-    form = float(json_object["form"])
-    ppg = float(json_object["points_per_game"])
+    form = float(json_object['form'])
+    ppg = float(json_object['points_per_game'])
     expected_points = max(form, ppg)
     injury_ratio = calculate_injury_multiplier(json_object)
     fixture_ratio = calculate_fixture_multiplier(
         json_object, json_fixture_object, gameweek)
-    result = expected_points * injury_ratio * fixture_ratio
+    past_fixture_ratio = calculate_past_fixture_multiplier(json_fixture_object)
+    result = expected_points * injury_ratio * fixture_ratio * past_fixture_ratio
     print('#predict_points({}..., {}..., {})'.format(json.dumps(json_object)
                                                      [:100], json.dumps(json_fixture_object)[:100], gameweek), result)
     return result
@@ -42,8 +43,8 @@ def calculate_injury_multiplier(json_object):
     is expected to score 10 points, but is only 50% likely to play, the expected points
     are adjusted to 10*0.5 = 5
     """
-    next_round_chance = json_object["chance_of_playing_next_round"] / 100 if json_object[
-        "chance_of_playing_next_round"] is not None else 1
+    next_round_chance = json_object['chance_of_playing_next_round'] / 100 if json_object[
+        'chance_of_playing_next_round'] is not None else 1
 
     print('#calculate_injury_multiplier({}...)'.format(
         json.dumps(json_object)[:100]), next_round_chance)
@@ -55,7 +56,7 @@ def calculate_fixture_multiplier(json_object, json_fixture_object, gameweek=0):
     Given a player's json fixture object, calculate a fixture multiplier for
     the expected points. This is calculated using each club's Elo rating.
     """
-    next_match = json_fixture_object["fixtures_summary"][gameweek]
+    next_match = json_fixture_object['fixtures_summary'][gameweek]
     team_id = json_object['team']
     opposition_team_id = next_match['team_a'] if next_match[
         'is_home'] else next_match['team_h']
@@ -67,3 +68,18 @@ def calculate_fixture_multiplier(json_object, json_fixture_object, gameweek=0):
     print('#calculate_fixture_multiplier({}..., {}..., {})'.format(
         json.dumps(json_object)[:100], json.dumps(json_fixture_object)[:100], gameweek), normalised_adjustment)
     return normalised_adjustment
+
+def calculate_past_fixture_multiplier(json_fixture_object):
+    """
+    Given a player's json fixture object, calculate a past fixture multiplier for
+    the expected points. This is a ratio of total minutes played divided by total
+    possible minutes. Prevents players being chosen who barely play but have a very
+    high points per game.
+    """
+    past_games = json_fixture_object['history_summary']
+    multiplier = 1
+    if len(past_games):
+        total_minutes = sum([fixture['minutes'] for fixture in past_games])
+        multiplier = total_minutes / (len(past_games) * 90)
+    print('#calculate_past_fixture_multiplier({}...)'.format(json.dumps(json_fixture_object)[:100]), multiplier)
+    return multiplier
